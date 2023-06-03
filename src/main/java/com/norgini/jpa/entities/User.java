204ef7a4b020ebf805374816f39c2b26e19fabea
jnorgini <jnorgini@gmail.com>
2023-06-03 00:42:24 -0300

    projeto finalizado

---
+++ src/main/java/com/norgini/jpa/entities/User.java
@@ -0,0 +1,30 @@
+package com.norgini.jpa.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private Double salary;
+
+}
